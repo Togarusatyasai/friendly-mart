6287e24733ec97b0ea1ca08f2c2422f5513c680c
Excelr <satya200228@gmail.com>
2024-07-28 19:48:52 +0530

    created

--- src/components/Vegetables.jsx
+++ src/components/Vegetables.jsx
@@ -3,7 +3,7 @@ import "./Vegetable.css";
 const Vegetables =()=>{
     return(
         <>
-        <header className="nav">
+        {/* <header className="nav">
         <div className="nav1">
         <img className="inav1" src="http://127.0.0.1:5501/fm1.png" />
         <div className="int">
@@ -67,7 +67,158 @@ const Vegetables =()=>{
     <div className="ft_ct">
         <p>Copyright@2024-2027.FriendlyMart...</p>
     </div>
-    </footer>
+    </footer> */}
+    <div className="veges">
+            <figure>
+              <a href="#"> 
+              <img className="images" src="https://tse4.mm.bing.net/th?id=OIP.OBKKA27v0VUeDTRMhzG_gAHaFX&pid=Api&P=0&h=180" alt="Sorry"/>
+              </a>
+              <figcaption>Tomato
+              <br></br>
+              ₹ 100/kg
+              </figcaption>
+            </figure>
+            <figure>
+              <a href="#">
+              <img className="images" src="http://seed2plant.in/cdn/shop/products/greenbrinjalseeds.jpg?v=1603446035" alt="Sorry"/>
+              </a>
+              <figcaption>Brinjal
+              <br></br>
+              ₹ 40/kg
+              </figcaption>
+            </figure>
+            <figure>
+              <a href="#">  
+              <img className="images" src="https://tse4.mm.bing.net/th?id=OIP.jmIWiX6tjIisDDykR3rWGgHaHa&pid=Api&P=0&h=180" alt="Sorry"/>
+              </a>
+              <figcaption>Cabbage 
+                <br></br>
+                ₹ 30/kg
+              </figcaption>
+            </figure>
+            <figure>
+             <a href="#"> 
+             <img className="images" src="https://tse4.mm.bing.net/th?id=OIP.qbtfTieEpOA4y66SY0zvEAHaHa&pid=Api&P=0&h=180" alt="Sorry"/>
+             </a>
+             <figcaption>Cauliflower
+             <br></br>
+             ₹ 30/kg
+             </figcaption>
+            </figure>
+            <figure>
+             <a href="#"> 
+             <img className="images" src="https://tse1.mm.bing.net/th?id=OIP.8m1P9KMHaWO2Wc_6iMGXiwAAAA&pid=Api&P=0&h=180" alt="Sorry"/>
+             </a>
+             <figcaption>Bottle Gourd
+             <br></br>
+             ₹ 60/kg
+             </figcaption>
+            </figure>
+            <figure>
+             <a href="#"> 
+             <img className="images" src="https://rb.gy/at5dul" alt="Sorry"/>
+             </a>
+             <figcaption> Carrot
+             <br></br>
+             ₹ 35/kg
+             </figcaption>
+            </figure>
+            <figure>
+             <a href="#"> 
+             <img className="images" src="https://shorturl.at/QmbLc" alt="Sorry"/>
+             </a>
+             <figcaption> Beetroot
+             <br></br>
+             ₹ 120/kg
+             </figcaption>
+            </figure>
+            <figure>
+             <a href="#"> 
+             <img className="images" src="https://shorturl.at/5qKP6" alt="Sorry"/>
+             </a>
+             <figcaption>Potato
+             <br></br>
+             ₹ 50/kg
+             </figcaption>
+            </figure>
+            <figure>
+             <a href="#"> 
+             <img className="images" src="https://shorturl.at/HQaWx" alt="Sorry"/>
+             </a>
+             <figcaption>Spinach
+             <br></br>
+             ₹ 85/kg
+             </figcaption>
+            </figure>
+            <figure>
+             <a href="#"> 
+             <img className="images" src="https://shorturl.at/BEXAE" alt="Sorry"/>
+             </a>
+             <figcaption>Ladies Finger
+             <br></br>
+             ₹ 35/kg
+             </figcaption>
+            </figure>
+            <figure>
+             <a href="#"> 
+             <img className="images" src="https://th.bing.com/th/id/OIP.U0EYTacd3Og1ch5IrU9SigHaGB?w=212&h=180&c=7&r=0&o=5&pid=1.7" alt="Sorry"/>
+             </a>
+             <figcaption>Broccoli
+             <br></br>
+             ₹ 210/kg
+             </figcaption>
+            </figure>
+            <figure>
+             <a href="#"> 
+             <img className="images" src="https://shorturl.at/OAspV" alt="Sorry"/>
+             </a>
+             <figcaption>Cucumber
+             <br></br>
+             ₹ 50/kg
+             </figcaption>
+            </figure>
+            <figure>
+             <a href="#"> 
+             <img className="images" src="https://shorturl.at/FKXM6" alt="Sorry"/>
+             </a>
+             <figcaption>Green Chillies
+             <br></br>
+             ₹ 80/kg
+             </figcaption>
+            </figure>
+            <figure>
+             <a href="#"> 
+             <img className="images" src="https://shorturl.at/0q45D" alt="Sorry"/>
+             </a>
+             <figcaption>Bitter Gourd
+             <br></br>
+             ₹ 90/kg
+             </figcaption>
+            </figure>
+            <figure>
+             <a href="#"> 
+             <img className="images" src="https://shorturl.at/j1Pq7" alt="Sorry"/>
+             </a>
+             <figcaption>Beans
+             <br></br>
+             ₹ 80/kg
+             </figcaption>
+            </figure>
+            <figure>
+             <a href="#"> 
+             <img className="images" src="https://shorturl.at/q1RiJ" alt="Sorry"/>
+             </a>
+             <figcaption>Raddish
+             <br></br>
+             ₹ 50/kg
+             </figcaption>
+            </figure>
+            
+            
+            
+            </div>
+       
+
         </>
     );
 }
