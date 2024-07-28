4f4e9445d69840089cc1a01bd0784313e287870b
Excelr <satya200228@gmail.com>
2024-07-28 19:41:42 +0530

    created

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -138,7 +138,7 @@ const Home = ()=> {
 <hr/>
 <footer>
     <div className="foot">
-        <div className="f1"><img className="f11" src="file:///C:/Users/SATYA/OneDrive/Pictures/mini%20photos/icon.png" alt="logo"/></div>
+        <div className="f1"><img className="f11" src="https://i.ibb.co/6r90CJ9/icon.png" alt="logo"/></div>
         <div className="f2">
             <p className="para"><i className="fa-solid fa-location-dot"></i>Jubilee Hills,RoadNo:7,Hyderabad,Telanagana</p>
             <p className="para"><i className="fa-solid fa-phone"></i>+91 9123456871</p>
