5d88a8e6d647696744bc82b95718fdf2fe99fd81
Excelr <satya200228@gmail.com>
2024-07-28 19:42:11 +0530

    created

--- src/main.jsx
+++ src/main.jsx
@@ -4,5 +4,5 @@ import App from './App.jsx'
 import Home from './components/Home.jsx'
 import Vegetables from './components/Vegetables.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Home></Home>
+  <Vegetables></Vegetables>
 )
